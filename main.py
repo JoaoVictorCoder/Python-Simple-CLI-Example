9d5f70de42cdd8f847b135a8ec796e21b5496db8
JoaoVictorCoder <jvrd1515@gmail.com>
2025-08-31 19:29:26 -0300

    :sparkles: feat: add postcmd

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 from colorama import Fore
 from prettytable import PrettyTable, TableStyle
 
-list = ["item 1", "item 2", "item 3", "item 4"]
 fm = Fore.MAGENTA
 fy = Fore.YELLOW
 fr = Fore.RESET
@@ -30,17 +29,20 @@ class MyCLI(cmd.Cmd):
     intro = header.get_string()
 
     def precmd(self, line):
+        print()
         return line
 
     def do_hello(self, line):
-        print("Hello!")
+        print("hello")
 
     def do_q(self, line):
+        "Quit CLI"
         print("Bye Bye :(")
         return True
 
-    def q(self):
-        print("It quits")
+    def postcmd(self, stop, line):
+        print()
+        return stop
 
 
 if __name__ == "__main__":
